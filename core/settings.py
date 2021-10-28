60a634aee914110e67c37ca374c1aa8133e69713
Adriano Regis <adbrum@outlook.com>
2021-10-28 18:37:24 +0100

    Changed connection

--- core/settings.py
+++ core/settings.py
@@ -76,13 +76,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# Conectar localmente no sqlite
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-default_dburl = 'postgresql://{user}:{password}@{host}:5432/{database_name}'.format(
-    host=config('HOST'),
-    user=config('USER'),
-    password=config('PASSWORD'),
-    database_name=config('DATABASE_NAME'),
-)
+# default_dburl = 'postgresql://{user}:{password}@{host}:5432/{database_name}'.format(
+#     host=config('HOST'),
+#     user=config('USER'),
+#     password=config('PASSWORD'),
+#     database_name=config('DATABASE_NAME'),
+# )
 # default_dburl = 'postgresql://{user}:{password}@{host}:5432/{database_name}'.format(
 #     host='172.20.0.2',
 #     user='adbrum',
