fb6993acde371c9fee04f4d1adbc47923385e7c5
adbrum <adbrum@outlook.com>
2021-11-30 13:54:45 +0000

    Changed connection database

--- apps/app/views.py
+++ apps/app/views.py
@@ -158,7 +158,7 @@ def open_file_automodel(filename, item_value, periods, switch):
         df3 = pd.DataFrame.from_records(SAGRAData.objects.all().values())
 
     else:
-        SAGRAData.objects.all().delete()
+        # SAGRAData.objects.all().delete()
 
         df3 = pd.read_excel(f'core/static/files/upload/{filename}')
 
--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ from unipath import Path
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
